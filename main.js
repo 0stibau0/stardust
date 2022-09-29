60f4149219e293648d3974c1a1426c7ea19b8c58
0stibau0 <98879083+0stibau0@users.noreply.github.com>
2022-09-30 00:32:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 
-const question1 = document.querySelector(".firstQuestion");
-const question2 = document.querySelector(".secondQuestion");
-const openQuestion2 = document.querySelector(".answerA2");
-
-openQuestion2.addEventListener("click", () => {
-//     question1.style.transform = " translateY(-100px);"
-    question1.style.display = "none";
-    question2.style.display = "grid";
+    //connecting to our database
+    const url = "https://secondsemester-9317.restdb.io/rest/product?max=10";
+    const options = {
+      headers: {
+        "x-apikey": "6335d38b32330102d591d2b0",
+      },
+    };
     
-
-
-});
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((data) => handleProductList(data));
+    
+    //now we are fetching the data
+    function handleProductList(data) {
+      console.log(data);
+      data.forEach(showProduct);
+    }
+    
+    
