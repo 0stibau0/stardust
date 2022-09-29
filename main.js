e4e48aff58d9721e3f9b50ddd002eb9971e7d09e
0stibau0 <98879083+0stibau0@users.noreply.github.com>
2022-09-29 16:39:31 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const question1 = document.querySelector(".firstQuestion");
+const question2 = document.querySelector(".secondQuestion");
+const openQuestion2 = document.querySelector(".answerA2");
+
+openQuestion2.addEventListener("click", () => {
+//     question1.style.transform = " translateY(-100px);"
+    question1.style.display = "none";
+    question2.style.display = "grid";
+    
+
+
+});
